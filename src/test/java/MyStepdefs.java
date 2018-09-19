abfeb94c26f56ccf0104d6fc2ee01223fe07053d
fatihtokus <gulbahar.tokus@gmail.com>
2018-09-19 19:03:07 +0100

    test2

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,10 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @When("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,4 @@
+Feature: I can login to page
+
+  Scenario: Login with correct name and password
+    When I navigate to the login page
