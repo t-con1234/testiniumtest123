7160bb9b91ec44d88b9ad849230f13916103a53a
fatihtokus <gulbahar.tokus@gmail.com>
2018-10-05 21:18:43 +0100

    Became cloud ready

--- src/test/java/tcon/SeleniumTest.java
+++ src/test/java/tcon/SeleniumTest.java
@@ -3,81 +3,13 @@ package tcon;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import tcon.driver.TestBase;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest {
-
-    public static WebDriver newDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:/chrome-v2-33/chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
-        //WebDriver driver = new ChromeDriver(chromeOptions);
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-
-        return driver;
-    }
-
-    public static WebDriver newCloudDriver() throws MalformedURLException {
-        DesiredCapabilities dr=DesiredCapabilities.chrome();
-        dr.setBrowserName("chrome");
-        dr.setPlatform(Platform.WINDOWS);
-
-        RemoteWebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
-
-        return driver;
-    }
-
-    public static WebDriver newCloudDriverFromSauceLab() throws MalformedURLException {
-        DesiredCapabilities dr=DesiredCapabilities.chrome();
-        dr.setBrowserName("chrome");
-        dr.setPlatform(Platform.WINDOWS);
-
-        String USERNAME = "tcon";
-        String ACCESS_KEY = "0fb6bb0b-4b4d-485d-b5d3-e889e8d6eeb1";
-        String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
-
-
-        RemoteWebDriver driver=new RemoteWebDriver(new URL(URL), dr);
-
-        return driver;
-    }
-
-
-    public static final String USERNAME = "info";
-    public static final String ACCESS_KEY = "0237c87d1c462408827c3f97e45d806c";
-    public static final String KEY = USERNAME + ":" + ACCESS_KEY;
-    public static final String URL = "http://hub.testinium.io/wd/hub";
-
-    public static WebDriver newCloudDriver2() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("key", KEY);
-
-        capabilities.setCapability(CapabilityType.PLATFORM, "WIN10");
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
-        capabilities.setCapability(CapabilityType.VERSION, "LATEST");
-        capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
-        capabilities.setCapability("recordsVideo", true);
-        capabilities.setCapability("screenResolution", "SXGA");
-
-        WebDriver driver = new RemoteWebDriver(new URL(URL), capabilities);
-
-        return driver;
-    }
-
-
     @Test
     public void runTest() throws MalformedURLException {
 
@@ -86,7 +18,9 @@ public class SeleniumTest {
 
         WebDriver driver;
 
-        driver = newDriver();
+        driver = TestBase.getDriver();
+
+        //driver = TestBase.newDriver();
 
         //driver = newCloudDriverFromSauceLab();
 
--- src/test/java/tcon/driver/TestBase.java
+++ src/test/java/tcon/driver/TestBase.java
@@ -1,25 +1,108 @@
 package tcon.driver;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class DriverFactory {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
 
     // Get a new WebDriver Instance.
-    // There are various implementations for this depending on browser. The required browser can be set as an environment variable.
     // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
     public static WebDriver getDriver() {
 
-        String browser = System.getenv("BROWSER");
+        String browser = System.getProperty("BROWSER");
+        //String browser = System.getenv("BROWSER");
+        System.out.println("FFFFFFFFFFFFFFFFFff" + browser);
 
         if ("IE".equals(browser)) {
-            return new ChromeDriver();
+            throw new RuntimeException("IE is not available");
         }
         if ("FIREFOX".equals(browser)) {
-                return new ChromeDriver();
+            throw new RuntimeException("FIREFOX is not available");
         }
 
-        return new ChromeDriver();
 
+        DesiredCapabilities dr=DesiredCapabilities.chrome();
+        dr.setBrowserName("chrome");
+        dr.setPlatform(Platform.WINDOWS);
+        //dr.setPlatform(Platform.LINUX);
+
+        RemoteWebDriver driver = null;
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return driver;
+
+    }
+
+    public static WebDriver newDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:/chrome-v2-33/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
+        //WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+    public static WebDriver newCloudDriver() throws MalformedURLException {
+        DesiredCapabilities dr=DesiredCapabilities.chrome();
+        dr.setBrowserName("chrome");
+        dr.setPlatform(Platform.WINDOWS);
+
+        RemoteWebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
+
+        return driver;
+    }
+
+    public static WebDriver newCloudDriverFromSauceLab() throws MalformedURLException {
+        DesiredCapabilities dr=DesiredCapabilities.chrome();
+        dr.setBrowserName("chrome");
+        dr.setPlatform(Platform.WINDOWS);
+
+        String USERNAME = "tcon";
+        String ACCESS_KEY = "0fb6bb0b-4b4d-485d-b5d3-e889e8d6eeb1";
+        String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+
+
+        RemoteWebDriver driver=new RemoteWebDriver(new URL(URL), dr);
+
+        return driver;
+    }
+
+
+    public static final String USERNAME = "info";
+    public static final String ACCESS_KEY = "0237c87d1c462408827c3f97e45d806c";
+    public static final String KEY = USERNAME + ":" + ACCESS_KEY;
+    public static final String URL = "http://hub.testinium.io/wd/hub";
+
+    public static WebDriver newCloudDriver2() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("key", KEY);
+
+        capabilities.setCapability(CapabilityType.PLATFORM, "WIN10");
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        capabilities.setCapability(CapabilityType.VERSION, "LATEST");
+        capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
+        capabilities.setCapability("recordsVideo", true);
+        capabilities.setCapability("screenResolution", "SXGA");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), capabilities);
+
+        return driver;
     }
 }
